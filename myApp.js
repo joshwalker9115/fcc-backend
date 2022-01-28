74ef99545f84ed3ea6b35d1bd42171b0d405d677
Walker <jwalker083@su.edu>
2022-01-28 10:12:11 -0500

    serve index

--- myApp.js
+++ myApp.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World");
 
+const absolutePathIndex = __dirname + "/views/index.html";
+const sendIndex = (req, res) => res.sendFile(absolutePathIndex);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', sendIndex);
 
 
 
