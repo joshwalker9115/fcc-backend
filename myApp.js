d355b1e47aff523adc9a19ea8f38ab8b24865495
Walker <jwalker083@su.edu>
2022-01-28 14:17:50 -0500

    now serving /json

--- myApp.js
+++ myApp.js
@@ -5,13 +5,15 @@ console.log("Hello World");
 
 const absolutePathIndex = __dirname + "/views/index.html";
 const absolutePathCSS = __dirname + "/public";
+const jsonResponse = {"message": "Hello json"};
 
 const sendIndex = (req, res) => res.sendFile(absolutePathIndex);
-const middlewareCSS = () => express.static(absolutePathCSS);
-
+const middlewareCSS = express.static(absolutePathCSS);
+const sendJSON = (req, res) => res.json(jsonResponse);
 
 app.get('/', sendIndex);
-app.use('/public', express.static(__dirname + "/public"));
+app.use('/public', middlewareCSS);
+app.get('/json', sendJSON);
 
 
 
