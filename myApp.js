81bfb378496387783e6b06f5df2e055e1ebe50e1
Walker <jwalker083@su.edu>
2022-01-28 13:39:13 -0500

    add CSS

--- myApp.js
+++ myApp.js
@@ -4,9 +4,14 @@ var app = express();
 console.log("Hello World");
 
 const absolutePathIndex = __dirname + "/views/index.html";
+const absolutePathCSS = __dirname + "/public";
+
 const sendIndex = (req, res) => res.sendFile(absolutePathIndex);
+const middlewareCSS = () => express.static(absolutePathCSS);
+
 
 app.get('/', sendIndex);
+app.use*('/', middlewareCSS);
 
 
 
